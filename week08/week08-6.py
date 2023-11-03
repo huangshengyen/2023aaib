1a0401db791c4c182ed3e7ad60e386ff73738bcd
haungshengyen <0966549066sam@gmail.com>
2023-11-03 11:52:15 +0800

    week08

--- week08/week08-6.py
+++ week08/week08-6.py
@@ -3,11 +3,11 @@ class Solution:
         #先把week08-3 的倆倆比較寫出來
         N = len(arr) #小心，題目不是 a 而是 arr
 
-        for k in range(N):
-            for i in range(1,N):
-                if arr[i]<arr[i-1]:
-                    arr[i], arr[i-1] = arr[i-1], arr[i]
-
+        #for k in range(N):
+        #    for i in range(1,N):
+        #        if arr[i]<arr[i-1]:
+        #           arr[i], arr[i-1] = arr[i-1], arr[i]
+        arr.sort()
         for i in range(1,N):
             if arr[i] - arr[i-1] != arr[1] - arr[0]:
                 return False
