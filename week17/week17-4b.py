70b84cc98e2cca576e8962859b5de7464aa64307
Huangshengyen <0966549066sam@gmail.com>
2024-01-05 11:32:42 +0800

    week17

---
+++ week17/week17-4b.py
@@ -0,0 +1,6 @@
+# SOIR108_BASE_005
+a = int(input())
+ans = 0
+for i in range(1,a+1):
+	if a%i == 0: ans+=i
+print(ans,end='')
---
+++ week17/week17-4d.py
@@ -0,0 +1,4 @@
+#SOIT108_BASE_008(no)
+a = int(input())
+
+print(int(1.2*60*60/a),end='' )
